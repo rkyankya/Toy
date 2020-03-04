c65ddd4a42c76ab2d59d0d0272f4a0b784b9722d
rkyankya <rkyankya@gmail.com>
2020-03-04 18:43:40 +0300

    Static Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  get '/about', to: 'static_pages#about'
+  get '/help', to: 'static_pages#help'
+  get '/contact', to: 'static_pages#contact'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
